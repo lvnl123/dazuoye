f8704b3825ee4c1d851f20c40e648091fe46d773
lvnl123 <122267601+lvnl123@users.noreply.github.com>
2024-12-22 22:14:21 +0800

    Add files via upload

---
+++ C# maze-game/Labyrinth/SystemEx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labyrinth
+{
+    internal static class SystemEx
+    {
+        /// <summary>
+        /// 跨线程操作控件
+        /// </summary>
+        /// <param name="con"></param>
+        /// <param name="action"></param>
+        public static void ExecBeginInvoke(this Control con, Action action)
+        {
+            if (action == null) return;
+            if (con.InvokeRequired)
+            {
+                con.BeginInvoke(new Action(action));
+            }
+            else
+            {
+                action();
+            }
+        }
+        /// <summary>
+        /// 跨线程操作控件
+        /// </summary>
+        /// <param name="con"></param>
+        /// <param name="action"></param>
+        public static void ExecInvoke(this Control con, Action action)
+        {
+            if (action == null) return;
+            if (con.InvokeRequired)
+            {
+                con.Invoke(new Action(action));
+            }
+            else
+            {
+                action();
+            }
+        }
+    }
+}
